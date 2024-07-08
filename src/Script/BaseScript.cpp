ff0573740c86c9cb48be16cf1e055695bf4ce0ef
THEfireZz <maximedhaeyere@gmail.com>
2024-07-08 09:48:10 +0200

    BaseScript creation & copyRemoteScript

---
+++ src/Script/BaseScript.cpp
@@ -0,0 +1,39 @@
+//
+// Created by SD43247 on 05/07/2024.
+//
+
+#include <utility>
+#include <qfile.h>
+
+#include "BaseScript.h++"
+
+BaseScript::BaseScript(Job job, QString remoteScriptPath, QString remoteLaunchersPath, QString localJobLocation) : job_(
+        std::move(job)), remote_script_path_(std::move(remoteScriptPath)), remote_launchers_path_(
+        std::move(remoteLaunchersPath)), local_job_location_(std::move(localJobLocation)) {
+
+}
+
+void BaseScript::initialize() {
+    copyRemoteScript();
+}
+
+void BaseScript::copyRemoteScript() const {
+    // Copy the remote script to the local job location
+    QFile remote_script(remote_script_path_);
+    if (!remote_script.copy(local_job_location_ + job_.getJobName() + ".bat")) {
+        // throw exception
+        throw std::runtime_error("Could not copy the remote script to the local job location");
+    }
+
+    // Copy the remote launchers to the local job location
+    QFile remote_launchers(remote_launchers_path_);
+    if (!remote_launchers.copy(local_job_location_ + "lance.bat")) {
+        // throw exception
+        throw std::runtime_error("Could not copy the remote launchers to the local job location");
+    }
+
+}
+
+
+
+
---
+++ src/Script/BaseScript.h++
@@ -0,0 +1,27 @@
+//
+// Created by SD43247 on 05/07/2024.
+//
+
+#ifndef MYGALE_BASESCRIPT_H
+#define MYGALE_BASESCRIPT_H
+
+
+#include "../JobCreation/Job.h++"
+
+class BaseScript {
+public:
+    explicit BaseScript(Job job,QString remoteScriptPath, QString remoteLaunchersPath, QString localJobLocation);
+
+    void initialize();
+
+private:
+    Job job_;
+    QString remote_script_path_;
+    QString remote_launchers_path_;
+    QString local_job_location_;
+
+    void copyRemoteScript() const;
+};
+
+
+#endif //MYGALE_BASESCRIPT_H
